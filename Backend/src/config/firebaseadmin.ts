675b47690c96fba6d1208932642ba66156015c02
Shashini <shashiniimasha31@gmail.com>
2024-07-17 14:30:55 +0530

    Private key file added to the gitignore

--- Backend/src/config/firebaseadmin.ts
+++ Backend/src/config/firebaseadmin.ts
@@ -1,5 +1,5 @@
 import admin from 'firebase-admin';
-import * as serviceAccount from '../../panchikawatta-d9e2e-firebase-adminsdk-42fm8-e29b1feb43.json';
+import * as serviceAccount from '../../panchikawatta-d9e2e-firebase-adminsdk-42fm8-90e73f05c7.json';
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
