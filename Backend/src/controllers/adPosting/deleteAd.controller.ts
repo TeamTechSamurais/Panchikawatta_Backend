b4fbf15a951543ecb84b2361cab1534e3e190848
sheharaF <fernandopusmp.21@uom.lk>
2024-07-18 13:31:03 +0530

    Delete Ad controller made

---
+++ Backend/src/controllers/adPosting/deleteAd.controller.ts
@@ -0,0 +1,50 @@
+import { Request, Response } from 'express';
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+export const deleteServiceById = async (req: Request, res: Response) => {
+  const { serviceId } = req.params;
+
+  if (!serviceId) {
+    return res.status(400).json({ error: 'id is required' });
+  }
+
+  try {
+    const service = await prisma.service.delete({
+      where: {
+        serviceId: parseInt(serviceId),
+      },
+    });
+
+    res.status(200).json({ message: 'Service deleted successfully', service });
+  } catch (error:any) {
+    if (error.code === 'P2025') { // Prisma specific error code for not found
+      return res.status(404).json({ error: 'Service not found' });
+    }
+    res.status(500).json({ error: 'Internal server error', details: error.message });
+  }
+};
+
+export const deleteSparepartById = async (req: Request, res: Response) => {
+    const { sparePartId } = req.params;
+  
+    if (!sparePartId) {
+      return res.status(400).json({ error: 'id is required' });
+    }
+  
+    try {
+      const sparePart = await prisma.sparePart.delete({
+        where: {
+            sparePartId: parseInt(sparePartId),
+        },
+      });
+  
+      res.status(200).json({ message: 'Service deleted successfully', sparePart });
+    } catch (error:any) {
+      if (error.code === 'P2025') { // Prisma specific error code for not found
+        return res.status(404).json({ error: 'Service not found' });
+      }
+      res.status(500).json({ error: 'Internal server error', details: error.message });
+    }
+  };
--- Backend/src/routes/spare_parts/viewReminder.routes.ts
+++ Backend/src/routes/spare_parts/viewReminder.routes.ts
@@ -5,6 +5,6 @@ import { getVehiclesById } from "../../controllers/adListing/vehicle.Controller"
 
 export function configureVehicleRoutes(router: Router) {
     router.get('/getReminder/:userId', getUserVehicleReminders);
-    router.delete('/markAsDone/:vehicleId/:reminderType', markAsDone);
+    router.put('/markAsDone/:vehicleId/:reminderType', markAsDone);
     router.get('/getVehicles/:userId',getVehiclesById);
 }
